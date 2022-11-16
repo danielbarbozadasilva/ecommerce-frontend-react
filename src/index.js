87cee16ac1599460d5a21c6bcd49a7b278842642
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-16 18:13:05 -0300

    chore: add material ui theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routers from './routers.js'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import store from './store'
+import { Helmet } from 'react-helmet'
+import { ThemeProvider } from 'styled-components'
+import theme from './plugins/theme'
+import './assets/css/style.css'
+import ReduxToastr from './components/redux-toastr'
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const googleFontNunito =
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400&display=swap'
+const googleFontMontserrat =
+  'https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100&display=swap'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <ReduxToastr />
+    <ThemeProvider theme={theme}>
+      <Helmet>
+        <link rel="stylesheet" href={googleFontNunito} />
+        <link rel="stylesheet" href={googleFontMontserrat} />
+      </Helmet>
+      <Routers />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+)
+reportWebVitals()
