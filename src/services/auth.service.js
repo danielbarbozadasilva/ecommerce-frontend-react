ae63f6032610ffd56ed48557e032734890354008
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-17 14:21:11 -0300

    feat: ensure customer registration

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,4 @@
 import http from '../config/http'
 
 export const authService = (data) => http.post('/auth', data)
+export const registerService = (data) => http.post('/client', { ...data, auth: true })
