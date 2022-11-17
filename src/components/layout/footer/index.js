e9ee66f2d6ebaea9b567d0d498800dc9002670f0
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-17 10:51:16 -0300

    refactor: change footer style

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -1,71 +1,98 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 import {
   FaFacebookSquare,
   FaInstagramSquare,
-  FaWhatsappSquare
+  FaWhatsappSquare,
+  FaCcMastercard,
+  FaCcVisa,
+  FaBarcode
 } from 'react-icons/fa'
 import { MdLocationOn, MdMailOutline, MdPhone } from 'react-icons/md'
-import Image from '../../../assets/img/footer-image.png'
 import {
   SContainer,
   WebsiteRights,
-  ColNetworks,
+  ColCopyright,
   ColInfo,
-  SocialIconLink,
+  IconStyle,
   FooterLinkTitle,
   FooterName,
-  FooterImg
-} from '../styled'
-import { Row, Col } from 'react-bootstrap'
+  SContainerCopyright
+} from './styled'
 
-const Footer = (props) => {
+const Footer = () => {
   return (
-    <SContainer fluid>
-      <Row>
-        <ColNetworks>
-          <FooterLinkTitle>Redes sociais</FooterLinkTitle>
-          <SocialIconLink
-            href="https://www.facebook.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaFacebookSquare />
-          </SocialIconLink>
-          <SocialIconLink
-            href="https://www.instagram.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaInstagramSquare />
-          </SocialIconLink>
-          <SocialIconLink
-            href="https://api.whatsapp.com/send?phone=5521982187814"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaWhatsappSquare />
-          </SocialIconLink>
-        </ColNetworks>
-        <Col>
-          <FooterImg src={Image} />
-          <FooterName>Developed by Daniel Barboza</FooterName>
-          <FooterName>
-            Copyright © 2022 - Todos os direitos reservados
-          </FooterName>
-        </Col>
-        <ColInfo>
-          <WebsiteRights>
-            <MdLocationOn /> Endereço: Rua Gomes Yunes, 225
-          </WebsiteRights>
-          <WebsiteRights>
-            <MdMailOutline /> E-mail: contato@primetechecommerce.com
-          </WebsiteRights>
-          <WebsiteRights>
-            <MdPhone /> Telefone: +55 (21) 2125-0548
-          </WebsiteRights>
-        </ColInfo>
-      </Row>
-    </SContainer>
+    <>
+      <SContainer fluid>
+        <Row>
+          <Col>
+            <ColInfo>
+              <FooterLinkTitle>REDES SOCIAIS</FooterLinkTitle>
+              <IconStyle
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebookSquare />
+              </IconStyle>
+              <IconStyle
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaInstagramSquare />
+              </IconStyle>
+              <IconStyle
+                href="https://api.whatsapp.com/send?phone=5521982187814"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaWhatsappSquare />
+              </IconStyle>
+            </ColInfo>
+          </Col>
+
+          <Col>
+            <ColInfo>
+              <FooterLinkTitle>Informações</FooterLinkTitle>
+              <WebsiteRights>
+                <MdLocationOn /> Endereço: Rua Gomes Yunes, 225
+              </WebsiteRights>
+              <WebsiteRights>
+                <MdMailOutline /> E-mail: contato@primetech.com
+              </WebsiteRights>
+              <WebsiteRights>
+                <MdPhone /> Telefone: +55 (21) 2245-0548
+              </WebsiteRights>
+            </ColInfo>
+          </Col>
+
+          <ColInfo>
+            <FooterLinkTitle>FORMAS DE PAGAMENTO</FooterLinkTitle>
+            <IconStyle>
+              <FaCcMastercard />
+            </IconStyle>
+            <IconStyle>
+              <FaCcVisa />
+            </IconStyle>
+            <IconStyle>
+              <FaBarcode />
+            </IconStyle>
+          </ColInfo>
+        </Row>
+      </SContainer>
+
+      <SContainerCopyright fluid>
+        <Row>
+          <ColCopyright>
+            <FooterName>Developed by Daniel Barboza</FooterName>
+            <FooterName>
+              Copyright © 2021 - Todos os direitos reservados
+            </FooterName>
+          </ColCopyright>
+        </Row>
+      </SContainerCopyright>
+    </>
   )
 }
 
