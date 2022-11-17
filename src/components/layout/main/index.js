118a9f0f23e282708903c7e2e921d559604b20c1
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-17 10:54:02 -0300

    feat: create main layout

--- src/components/layout/main/index.js
+++ src/components/layout/main/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../header/index'
 import Footer from '../footer/index'
 import '../../../assets/css/style.css'
-import { Main } from '../styled'
+import { Main } from './styled'
 
 const Layout = (props) => {
   return (
---
+++ src/components/layout/main/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+  font-family: 'Nunito', Helvetica, Arial, Lucida, sans-serif;
+  font-size: 0.95rem;
+  line-height: 1.5;
+  font-weight: 500;
+  overflow: hidden;
+  flex: 1;
+  position: relative;
+  text-align: justify;
+  color: #473f57;
+  min-height: 100vh;
+`
