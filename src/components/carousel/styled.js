42f1aec571335dc66bdc5334dc98204211867f57
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-10 10:09:21 -0300

    feat: ensure the functioning of the carousel

---
+++ src/components/carousel/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import Carousel from 'react-bootstrap/Carousel';
+
+export const SCarousel = styled(Carousel)`
+   @media screen and (max-width: 850px) {
+        display: none;
+  }
+`
