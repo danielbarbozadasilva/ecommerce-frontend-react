3c0bfd27e30cbf8301641f9d56993c6f2042c764
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-15 17:30:33 -0300

    feat: ensure listing of product details

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,5 +1,5 @@
 import http from '../config/http'
 
 export const listAllCategoriesService = () => http.get('/category')
-export const listByIdCategoryService = (categoryid) =>
-  http.get(`/category/${categoryid}/products`)
+export const listByIdCategoryService = (categoryid, itemsPerPage, currentPage) =>
+  http.get(`/category/${categoryid}/products?itemsPerPage=${itemsPerPage}&page=${currentPage}`)
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,5 +1,10 @@
 import http from '../config/http'
+const baseUrl = '/product'
 
-export const listAllProductsService = () => http.get('/product')
-export const searchProductsService = (search) => http.get(`/product/search/${search}`)
-
+export const listAllProductsService = (itemsPerPage, currentPage) =>
+  http.get(`${baseUrl}?itemsPerPage=${itemsPerPage}&page=${currentPage}`)
+export const listByIdProductService = (id) => http.get(`${baseUrl}/${id}`)
+export const searchProductsService = (search, itemsPerPage, currentPage) =>
+  http.get(
+    `${baseUrl}/search/${search}?itemsPerPage=${itemsPerPage}&page=${currentPage}`
+  )
